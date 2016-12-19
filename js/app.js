9ae1f105499c3c1aa8c2159acedb89d22d3c6360
Yermolin Vlad <electrovladyslav@gmail.com>
2016-12-19 22:53:21 +0200

    no-js state was added to main-nav on mobile version

--- js/app.js
+++ js/app.js
@@ -1,6 +1,10 @@
 var mainNav = document.querySelector('.main-nav');
 var mainNavToggle = mainNav.querySelector('.main-nav__toggle');
 
+//Инициализация:
+mainNav.classList.remove('main-nav--opened');
+mainNav.classList.remove('main-nav--no-js');
+
 mainNavToggle.addEventListener('click', clickHandler);
 
 function clickHandler (event) {
