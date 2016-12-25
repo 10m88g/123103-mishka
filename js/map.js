cb80e8d119118eb0c3451b3ca297e4b3add7f032
Yermolin Vlad <electrovladyslav@gmail.com>
2016-12-25 17:22:46 +0200

    Dummy picture of google map for no-js state was done.

--- js/map.js
+++ js/map.js
@@ -1,6 +1,12 @@
 
 function initMap() {
-  var map = new google.maps.Map(document.querySelector('.contacts__map'), {
+  var mapContainer = document.querySelector(".contacts__map");
+
+  //удалим заглушку - picture и класс no-js
+  mapContainer.removeChild(mapContainer.firstElementChild);
+  mapContainer.classList.remove("contacts__map--no-js");
+
+  var map = new google.maps.Map(mapContainer, {
     zoom: 16,
     center: {lat: 59.936355, lng: 30.321623}
   });
